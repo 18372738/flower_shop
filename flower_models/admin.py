834ee00c7089054fce1843201c0f32f400af41b5
18372738 <133884450+18372738@users.noreply.github.com>
2024-09-18 01:11:59 +0300

    Start project flower_shop

---
+++ flower_models/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Event, Bouquet, Order
+
+
+admin.site.register(Event)
+admin.site.register(Bouquet)
+admin.site.register(Order)
---
+++ flower_models/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+
+ORDER_CHOICES = (
+    ("true", "подтверждение"),
+    ("courier", "передан курьеру"),
+    ("delivered", "доставлено")
+)
+
+
+class Event(models.Model):
+    """События"""
+    title = models.CharField("Название события", max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Bouquet(models.Model):
+    """Букеты"""
+    title = models.CharField("Название букета", max_length=200, unique=True)
+    price = models.FloatField("Стоимость", blank=True)
+    event = models.ManyToManyField(
+        Event,
+        verbose_name="Событие",
+        related_name="events",
+    )
+    photo = models.ImageField("Фото")
+    description = models.TextField("Описание", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Order(models.Model):
+    """Заказ"""
+    full_name = models.CharField("ФИО", max_length=200)
+    phone = models.CharField('Телефон', max_length=200)
+    address = models.CharField("Адрес", max_length=200)
+    delivery_date = models.DateTimeField("Дата доставки")
+    bouquet = models.ForeignKey(
+        Bouquet,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE,
+    )
+    status = models.CharField(
+        verbose_name="Статус",
+        choices=ORDER_CHOICES,
+        max_length=30,
+        default="true",
+    )
+
+    def __str__(self):
+        return self.full_name

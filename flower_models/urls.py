7cc522d4a63a304c33cfe95437ac6588ac91eded
damirbakty <damir_bakty@mail.ru>
2024-09-18 11:11:59 +0500

    add static and templates

---
+++ flower_models/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from flower_models.views import index, card, consultation, order, order_step, quiz, result, catalog
+
+app_name = 'flower_models'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('card', card, name='card'),
+    path('consultation', consultation, name='consultation'),
+    path('order', order, name='order'),
+    path('order_step', order_step, name='order_step'),
+    path('quiz', quiz, name='quiz'),
+    path('result', result, name='result'),
+    path('catalog', catalog, name='catalog')
+]
--- flower_models/views.py
+++ flower_models/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# TODO
+def index(request):
+    return render(request, 'flowers_models/index.html')
+
+
+# TODO
+def card(request):
+    return render(request, 'flowers_models/card.html')
+
+
+# TODO
+def consultation(request):
+    return render(request, 'flowers_models/consultation.html')
+
+
+# TODO
+def order(request):
+    return render(request, 'flowers_models/order.html')
+
+
+# TODO
+def order_step(request):
+    return render(request, 'flowers_models/order-step.html')
+
+
+# TODO
+def quiz(request):
+    return render(request, 'flowers_models/quiz.html')
+
+
+# TODO
+def result(request):
+    return render(request, 'flowers_models/result.html')
+
+# TODO
+def catalog(request):
+    return render(request, 'flowers_models/catalog.html')

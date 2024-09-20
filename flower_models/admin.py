d14ceae32aa29f2cf9f9159a9cc4db96553f5ceb
18372738 <133884450+18372738@users.noreply.github.com>
2024-09-20 06:14:52 +0300

    Added model Client a db

--- flower_models/admin.py
+++ flower_models/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Event, Bouquet, Order
+from .models import Event, Bouquet, Order, Client
 
 
 admin.site.register(Event)
 admin.site.register(Bouquet)
 admin.site.register(Order)
+admin.site.register(Client)
